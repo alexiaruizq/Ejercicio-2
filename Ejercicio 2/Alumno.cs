ff8abb5138ecde3f2aedbac4db76d57543969768
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-22 07:42:56 -0700

    Agregar archivos de proyecto.

---
+++ Ejercicio 2/Alumno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Alumno
+    {
+
+        public string Nombre { get; set; }
+        public string Matricula { get; set; }
+        public string Nacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+
+        public Alumno()
+        {
+
+            Nombre = "Alexia";
+            Matricula = "189363";
+        }
+            public Alumno(string nombre, string matricula)
+            {
+                Nombre = nombre;
+                Matricula = matricula;
+
+
+            }
+        }
+    }
---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno1 = new Alumno();
+
+            Console.WriteLine("Alumno ");
+            Console.WriteLine("Nombre: " + alumno1.Nombre);
+            Console.WriteLine("Matricula " + alumno1.Matricula);
+            Console.WriteLine("Correo: ");
+            Console.WriteLine("Direccion: ");
+
+            Alumno alumno2 = new Alumno("Alexia", "alexiaruiz-@hotmail.com");
+
+            Console.Read();
+
+        }
+    }
+}
